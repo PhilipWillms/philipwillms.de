421d26f57f760ad871164e5ee90422dc3c702480
Philip Willms <philip@narando.de>
2020-02-16 12:27:24 +0100

    [description] add responsiveness

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import Img from "gatsby-image";
 
 export const Description = props => (
-  <div className="flex flex-col items-center w-6/7 mt-20">
-    <Img
-      title="Product image"
-      className="rounded-full w-24 h-24"
-      fluid={props.headerImage.childImageSharp.fluid}
-    />
-    <h1 className="text-xl mt-3 mb-1">
-      {props.descriptionText.frontmatter.name}
-    </h1>
-    <div className="text-gray-400 text-sm">
-      <a className="italic">Email</a> | 
-      <a className="italic"> Github</a> |<a className="italic"> Gitlab</a>
+  <div className="flex flex-col items-center w-6/7 mt-20 sm:w-5/6">
+    <div className="flex flex-col items-center w-full sm:flex-row">
+      <Img
+        title="Product image"
+        className="rounded-full w-24 h-24"
+        fluid={props.headerImage.childImageSharp.fluid}
+      />
+      <h1 className="text-xl mt-3 mb-1 sm:m-0 sm:ml-4 sm:font-medium sm:mb-4">
+        {props.descriptionText.frontmatter.name}
+      </h1>
+      <div className="text-gray-400 text-sm sm:ml-auto sm:mb-4 sm:text-base">
+        <a className="italic">Email</a> | 
+        <a className="italic"> Github</a> |<a className="italic"> Gitlab</a>
+      </div>
     </div>
-    <p className="text-center text-lg mt-5 font-hairline text-gray-400 leading-relaxed tracking-wide">
+    <p className="text-center text-lg mt-5 font-hairline text-gray-400 leading-relaxed tracking-wide sm:text-left sm:mr-auto sm:text-xl sm:max-w-2xl">
       {props.descriptionText.frontmatter.descriptionText}
     </p>
   </div>
 );
-
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,20 +10,22 @@ import Projects from "../markdown-pages/project.yaml";
 import leptop from "../images/leptop.ico";
 
 const IndexPage = props => (
-  <div className="flex flex-col items-center bg-gray-100">
-    <Helmet>
-      <link rel="icon" type="image/png" href={leptop} sizes="32x32" />
-      <title>Philip Willms</title>
-    </Helmet>
-    <Description
-      descriptionText={props.data.descriptionContent}
-      headerImage={props.data.headerImage}
-    />
-    <Header header="My Projects" />
-    {Projects.map((project, index) => {
-      return <Project key={index} projectData={project} />;
-    })}
-    <Footer bielefeld={props.data.bielefeld} />
+  <div className="flex items-center w-full justify-center bg-gray-100">
+    <div className="flex flex-col w-full items-center bg-gray-100 max-w-6xl">
+      <Helmet>
+        <link rel="icon" type="image/png" href={leptop} sizes="32x32" />
+        <title>Philip Willms</title>
+      </Helmet>
+      <Description
+        descriptionText={props.data.descriptionContent}
+        headerImage={props.data.headerImage}
+      />
+      <Header header="My Projects" />
+      {Projects.map((project, index) => {
+        return <Project key={index} projectData={project} />;
+      })}
+      <Footer bielefeld={props.data.bielefeld} />
+    </div>
   </div>
 );
 
