e4f29c1f3d6f4bae0c61cb2de98ea9dad4377fe4
Philip Willms <philip@instantcommerce.io>
2023-10-24 17:25:11 +0200

    feat: improve title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Philip Willms`,
+    title: `Philip Willms - Frontend Developer`,
     description: ` While my journey into tech may not have been a lifelong passion, I
     quickly fell in love with the way of thinking coding demands. The
     combination of implementing complex logic with the artful translation of
